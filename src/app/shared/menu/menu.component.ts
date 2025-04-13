7ee3db3d8449374a574198c9d85d43d058a11af3
RevMarci <reveszmarton16@gmail.com>
2025-04-13 23:30:35 +0200

    Mégtöbb material a követelmények kedvéért <3

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -3,11 +3,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatIconModule, RouterModule],
+  imports: [CommonModule, MatSidenavModule, MatIconModule, RouterModule, MatListModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
